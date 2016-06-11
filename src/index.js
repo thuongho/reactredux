f825726652f7853d617a78cc29e04a32d04624be
Thuong Ho <thuong.t.ho@gmail.com>
2016-06-10 17:57:58 -0700

    setup entry point index.js to utilize react router

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('hi');
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, browerHistory } from 'react-router';
+import routes from './routes';
+import './styles/styles.css'; //Webpack can import CSS files too!
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+render(
+    <Router history={browerHistory} routes={routes} />,
+    document.getElementById('app')
+);
